6f618a55a9e538dce6fb2b82173a509b38f7dfd6
iamsatyanchal <quickhalewebtech@gmail.com>
2026-06-15 22:53:54 +0530

    initiated the bot structure

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,26 @@
 const std = @import("std");
 const memory = std.mem;
 const show = std.debug.print;
+const fmt = std.fmt.allocPrint;
+const heap = std.heap;
 
-pub fn main() !void {
-    show("starting", {});
-}
+pub const Bot = struct {
+    bot_token: []const u8,
+    base_url: []const u8,
+    allocator: memory.Allocator,
+
+    pub fn init(allocator: memory.Allocator, bot_token: []const u8) !Bot {
+        const base_url = try fmt(allocator, "https://api.telegram.com/bot{s}", .{bot_token});
+        return Bot{
+            .bot_toke = bot_token,
+            .base_url = base_url,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Bot) void {
+        defer self.allocator.free(self.base_url);
+    }
+};
+
+pub fn main() !void {}
