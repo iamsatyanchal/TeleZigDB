c8b586097206f2ad05d8e1b8e81c181372a22929
iamsatyanchal <quickhalewebtech@gmail.com>
2026-06-18 14:25:25 +0530

    debugging in production wtfff

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ app.get("/*url", async (req, res)=> {
     var original_data;
     console.log(fetching_url);
 
-    try {
-        original_data = await fetch(fetching_url.slice(1)).then(response => response.json());
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json(
-            {
-                success: false,
-            });
-    }
+    // try {
+    //     original_data = await fetch(fetching_url.slice(1)).then(response => response.json());
+    // } catch (error) {
+    //     console.log(error);
+    //     return res.status(500).json(
+    //         {
+    //             success: false,
+    //         });
+    // }
     return res.status(200).json(
         {
             success: true,
             url: fetching_url,
-            data: original_data
+            // data: original_data
         }
     );
 });
